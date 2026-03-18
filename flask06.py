e47be8e23cbadbe31bf90902f2171c217bba00ed
구본무 <kubonmo519@gmail.com>
2026-03-18 17:22:13 +0900

    사용자 지향적이게 html 수정,

--- flask06.py
+++ flask06.py
@@ -2,19 +2,24 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=["GET","POST"])
 def home():
-    result = None # 아니 뭔 미리 NOne 이라고 정의해야함? 뭔 이딴 모듈이 다있음???????????????????????????
+    
+    avg_result = 0 
+    max_result = 0
+    min_result = 0
+    
     if request.method == "POST":
         num1 = int(request.form['number01']) # !!!!!!!! HTML에서 값 가져오기@@@@@@@@@@@@@@
-        
         num2 = int(request.form['number02']) # !!!!!!!! HTML에서 값 가져오기@@@@@@@@@@@@@@
-
-        num3 = int(request.form['number02']) # !!!!!!!! HTML에서 값 가져오기@@@@@@@@@@@ 정수값으로 반환해야함
+        num3 = int(request.form['number03']) # !!!!!!!! HTML에서 값 가져오기@@@@@@@@@@@ 정수값으로 반환해야함
         
-        result =( num1+num2+num3)/ 3
-    elif request.method=="GET":
-        return "homeP"
+        avg_result = round((num1+num2+num3)/ 3) #라운드 함수, 반올림
+        max_result = max(num1,num2,num3)
+        min_result = min(num1,num2,num3)
+    #elif request.method=="GET":
+    #    return "homeP" HTML에서 GET메서드를 못가져옴, 곧 수정예정??
 
-    return render_template("avg.html", result=result)
+    return render_template("avg.html", avg_result=avg_result,max_result=max_result,min_result=min_result)
 app.run(host="0.0.0.0",port=5000)
