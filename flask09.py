fda0c2f303cb12c43173c0ac9152817bd6faccef
구본무 <kubonmo519@gmail.com>
2026-03-20 16:35:48 +0900

    커뮤니티 pw 추가

--- flask09.py
+++ flask09.py
@@ -1,4 +1,5 @@
 import sqlite3  # sqlite DB 쓰려고 불러옴
+import datetime # 현재시간을 출력하게,
 from flask import Flask, request, render_template, redirect
 
 app = Flask(__name__)
@@ -10,7 +11,9 @@ def init_db():
         CREATE TABLE IF NOT EXISTS posts (  -- posts 테이블이 없으면 생성
             id INTEGER PRIMARY KEY AUTOINCREMENT,  -- 글 번호(자동 증가)
             nickname TEXT NOT NULL,  -- 닉네임
-            contents TEXT NOT NULL  -- 글 내용
+            contents TEXT NOT NULL,  -- 글 내용
+            password TEXT NOT NULL,
+            time TEXT NOT NULL
         )
     """)
     conn.commit()  # 저장
@@ -18,18 +21,22 @@ def init_db():
 
 @app.route("/", methods=["GET", "POST"])
 def home():
+    
     is_null = ""
     conn = sqlite3.connect("community.db")  # DB 연결
     c = conn.cursor()  # SQL 실행 도구
     
     if request.method == "POST":
+        time = datetime.datetime.now()
         nickname = request.form["nickName"]
         contents = request.form["contents"]
+        password = request.form["password"]
         
-        if nickname != "" and contents != "":
+
+        if nickname != "" and contents != "" and password != "":
             c.execute(
-            "INSERT INTO posts (nickname, contents) VALUES (?, ?)",  # 글 추가
-            (nickname, contents)
+            "INSERT INTO posts (nickname, contents, password, time) VALUES (?, ?, ?, ?)",  # 글 추가
+            (nickname, contents, password, time)
         )
             conn.commit()  # 추가한 내용 저장
             conn.close()  # DB 닫기
@@ -55,4 +62,4 @@ def delete_all():
     return redirect("/")
 
 init_db()  # 처음 실행할 때 테이블 준비
-app.run(host="0.0.0.0", port=5000)
+app.run(host="0.0.0.0", port=5000, debug=True)
